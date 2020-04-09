f53bdc383a6dfbef6541421e87086332f5905baf
aminaouini <aminaouini565@gmail.com>
2020-04-09 15:32:00 +0200

    i need a some time to finish

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost:27017/challenge_4`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', err => {
+    throw err;
+});
+
+mongoose.connection.once('open', () => console.log('mongoose connected'))
+
+app.use(bodyParser.json());
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`)
+});
